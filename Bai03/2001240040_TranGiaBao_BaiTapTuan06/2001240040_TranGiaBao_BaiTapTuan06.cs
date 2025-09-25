c8ad5dbb714366ed5e04c5e15e81a12a2e2f5443
Tran Gia Bao <g.baotran06@gmail.com>
2025-09-25 18:31:54 +0700

    BaiTap03

---
+++ Bai03/2001240040_TranGiaBao_BaiTapTuan06/2001240040_TranGiaBao_BaiTapTuan06.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiChuyenXe
+{
+    public class ChuyenXe
+    {
+        protected string MaChuyen, HoTenTX;
+        protected int SoXe;
+        protected double DoanhThu;
+        public ChuyenXe ()
+        {
+            MaChuyen = "Chua co";
+            HoTenTX = "Chua co";
+            SoXe = 0;
+            DoanhThu = 0;
+        }
+        public ChuyenXe (string MaChuyen, string HoTenTX, int SoXe, double DoanhThu)
+        {
+            this.MaChuyen = MaChuyen;
+            this.HoTenTX = HoTenTX;
+            this.SoXe = SoXe;
+            this.DoanhThu = DoanhThu;
+        }
+        public void Nhap()
+        {
+            Console.Write("Nhap ma chuyen: ");
+            MaChuyen = Console.ReadLine();
+
+            Console.Write("Nhap ten tai xe: ");
+            HoTenTX = Console.ReadLine();
+
+            Console.Write("Nhap so xe: ");
+            SoXe = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhap doanh thu: ");
+            DoanhThu = double.Parse(Console.ReadLine());
+        }
+        public void Xuat()
+        {
+            Console.WriteLine("Ma chuyen: "+MaChuyen);
+            Console.WriteLine("Ten tai xe: "+HoTenTX);
+            Console.WriteLine("So xe: "+SoXe);
+            Console.WriteLine("Doanh Thu: "+DoanhThu);
+        }
+    }
+    public class ChuyenXeNoiThanh : ChuyenXe
+    {
+        int SoTuyen;
+        double SoKm;
+        public ChuyenXeNoiThanh():base()
+        {
+            SoTuyen = 0;
+            SoKm = 0;
+        }
+        public ChuyenXeNoiThanh(string MaChuyen, string HoTenTX, int SoXe, double DoanhThu, int SoTuyen, double SoKm) : base(MaChuyen, HoTenTX, SoXe, DoanhThu)
+        {
+            this.SoTuyen = SoTuyen;
+            this.SoKm = SoKm;
+        }
+        public new void Nhap()
+        {
+            base.Nhap();
+            Console.Write("Nhap so tuyen: ");
+            SoTuyen = int.Parse(Console.ReadLine());
+            Console.Write("Nhap so km: ");
+            SoKm = double.Parse(Console.ReadLine());
+        }
+        public new void Xuat()
+        {
+            base.Xuat();
+            Console.WriteLine("So tuyen: "+ SoTuyen);
+            Console.WriteLine("So Km: "+SoKm);
+        }
+    }
+    public class ChuyenXeNgoaiThanh : ChuyenXe
+    {
+        string NoiDen;
+        double SoNgayDiDuoc;
+        public ChuyenXeNgoaiThanh() : base()
+        {
+            NoiDen = "Chua co";
+            SoNgayDiDuoc = 0;
+        }
+        public ChuyenXeNgoaiThanh(string MaChuyen, string HoTenTX, int SoXe, double DoanhThu, string NoiDen, double SoNgayDiDuoc) : base(MaChuyen, HoTenTX, SoXe, DoanhThu)
+        {
+            this.NoiDen = NoiDen;
+            this.SoNgayDiDuoc = SoNgayDiDuoc;
+        }
+        public void Nhap()
+        {
+            base.Nhap();
+            Console.Write("Nhap noi den: ");
+            NoiDen = Console.ReadLine();
+            Console.Write("Nhap so ngay di duoc: ");
+            SoNgayDiDuoc = double.Parse(Console.ReadLine());
+        }
+        public void Xuat()
+        {
+            base.Xuat();
+            Console.WriteLine("Noi den: " + NoiDen);
+            Console.WriteLine("So ngay di duoc: "+SoNgayDiDuoc);
+        }
+    }
+    public class ChuongTrinh
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("--- Chuyen xe noi thanh ---");
+            ChuyenXeNoiThanh CXNoiT = new ChuyenXeNoiThanh();
+            CXNoiT.Nhap();
+            CXNoiT.Xuat();
+            Console.WriteLine("--- Chuyen xe nogai  thanh ---");
+            ChuyenXeNgoaiThanh CXNgoaiT = new ChuyenXeNgoaiThanh();
+            CXNgoaiT.Nhap();
+            CXNgoaiT.Xuat();
+        }
+    }
+}
---
+++ Bai03/2001240040_TranGiaBao_BaiTapTuan06/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("2001240040_TranGiaBao_BaiTapTuan06")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("2001240040_TranGiaBao_BaiTapTuan06")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cfa0b4c7-6c73-4548-b788-d518744b07ee")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
